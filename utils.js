ae54277b962cad95331381b7059a06bce6411e24
Igor Owczarz <98612273+Ajgor-idk@users.noreply.github.com>
2022-09-05 23:05:25 +0200

    Not the best piece of code some things can definitely be better I also forgot to do the commits cuz I thought it's gonna take me a little less of code and time.

---
+++ utils.js
@@ -0,0 +1,3 @@
+export const querySelect = (elem) => document.querySelector(elem);
+export const querySelectAll = (elem) => document.querySelectorAll(elem);
+export const createElem = (elem) => document.createElement(elem);
