18ffed3bb7800c204ae5dded33be7eaa3ed480b5
Igor Owczarz <98612273+Ajgor-idk@users.noreply.github.com>
2022-09-08 16:55:56 +0200

    Removed files so we can compare them with another branch with code.

--- utils.js
+++
@@ -1,3 +0,0 @@
-export const querySelect = (elem) => document.querySelector(elem);
-export const querySelectAll = (elem) => document.querySelectorAll(elem);
-export const createElem = (elem) => document.createElement(elem);
